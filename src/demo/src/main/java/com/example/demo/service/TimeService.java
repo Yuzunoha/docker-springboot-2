5debe56c7a5008bd3559e68f03147102f3be96f5
Yuzunoha <studysteadystandby@gmail.com>
2025-01-27 19:52:48 +0900

    できた。JST文字列が出るようになったよ

--- src/demo/src/main/java/com/example/demo/service/TimeService.java
+++ src/demo/src/main/java/com/example/demo/service/TimeService.java
@@ -1,11 +1,22 @@
 package com.example.demo.service;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class TimeService {
 
   public String time() {
-    return "time()です。";
+    // タイムゾーン指定のオブジェクトを生成する
+    ZonedDateTime jstZonedDateTime = ZonedDateTime.now(ZoneId.of("Asia/Tokyo"));
+
+    // "2025/01/27 19:49:38 JST" の形に変換する
+    String s = jstZonedDateTime.format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss")) + " JST";
+
+    // 返却する
+    return s;
   }
 }
