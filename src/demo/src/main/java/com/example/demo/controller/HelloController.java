66baece73a628a124d6ed45a8423e462b911c678
Yuzunoha <studysteadystandby@gmail.com>
2025-01-27 19:20:41 +0900

    @RequiredArgsConstructor 成功

--- src/demo/src/main/java/com/example/demo/controller/HelloController.java
+++ src/demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,14 +1,18 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.HelloService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class HelloController {
+
+  private final HelloService helloService;
 
   @RequestMapping({ "/", "hello" })
   public String hello() {
-    return "hello! こんにちは。";
+    return helloService.hello();
   }
-
 }
