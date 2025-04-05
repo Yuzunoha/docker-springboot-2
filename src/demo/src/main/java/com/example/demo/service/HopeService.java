925501b8794b7284517b0b8081b326cdd7a346d6
Yuzunoha <studysteadystandby@gmail.com>
2025-04-05 21:09:22 +0900

    メルカリで中古のiPad Pro 11インチ M2 買いました。満足です！

--- src/demo/src/main/java/com/example/demo/service/HopeService.java
+++ src/demo/src/main/java/com/example/demo/service/HopeService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 public class HopeService {
   public String hope() {
     var s = "iPad Air 11インチ Wi-Fi 128GB 2025年春モデル MC9X4J/A [ブルー]";
+    s = "メルカリで中古のiPad Pro 11インチ M2 買いました。満足です！";
     return s;
   }
 }
